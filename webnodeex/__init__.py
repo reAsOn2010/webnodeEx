f823a2a441a083391923b647ef060cfdbc5e03de
reAsOn2010 <the.reason.sake@gmail.com>
2014-12-17 18:02:55 +0800

    yield children

--- webnodeex/__init__.py
+++ webnodeex/__init__.py
@@ -45,16 +45,18 @@ class WebNodeEx(object):
 
     def __init__(self):
         self.var2key = {}
+        self.key2child = {}
         self.key2value = {}
         self._iterator = None
+        self._children = None
         self.current_level_key2value = {}
         self.center = ClientCenter(self)
 
-    def children(self):
-        raise NotImplementedError()
+    def register(self):
+        yield {}
 
-    def obj(self):
-        raise NotImplementedError()
+    def children(self):
+        yield {}
 
     def submit(self, call_tuple):
         key = call_tuple.generate_key()
@@ -75,11 +77,25 @@ class WebNodeEx(object):
         self.var2key.update(next(self._iterator, {}))
         return self.current_level_key2value
 
+    def nurture(self):
+        if self._children is None:
+            self._children = self.children()
+        self.key2child.update(next(self._children, {}))
+        return self.key2child.values()
+
     def __getattr__(self, name):
         client = self.center.get(name)
         if not client:
-            return self.key2value.get(self.var2key.get(name, None), None)
+            return self._fetch(name)
         return client
+
+    def _fetch(self, name):
+        value = self.var2key.get(name, None)
+        if not value:
+            return None
+        if isinstance(value, list):
+            return map(lambda x: self.key2value.get(x, None), value)
+        return self.key2value.get(value, None)
 
 
 class WebNodeExRoot(WebNodeEx):
@@ -92,20 +108,23 @@ class WebNodeExRoot(WebNodeEx):
         self.all_key2value = {}
 
     def gao(self):
-        nodes = self.children()
+        nodes = self.nurture()
         while nodes:
             next_level_nodes = []
             current_key2value = {}
 
             for node in nodes:
+                print node
                 key2value = node.next()
                 if not key2value:
-                    next_level_nodes.extend(node.children())
+                    next_generation = node.nurture()
+                    if next_generation:
+                        next_level_nodes.extend(next_generation)
                 else:
                     next_level_nodes.append(node)
                 current_key2value.update(key2value)
 
-            # TODO: aggregate and filter duplicate fetch
+            # aggregate and filter duplicate fetch
             current_key2value.update((key, self.all_key2value[key]) for key in current_key2value.viewkeys() & self.all_key2value.viewkeys())
 
             print current_key2value
