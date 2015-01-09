36bb21fc5f136806f29b25b1836ccd8fa593e742
reAsOn2010 <the.reason.sake@gmail.com>
2015-01-09 20:57:17 +0800

    split into large and medium yield

--- webnodeex/dummy.py
+++ webnodeex/dummy.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tornado.util import ObjectDict
-
-from ex import WebNodeEx, WebNodeExRoot
-from drivers import RedisFetchDriver, RPCFetchDriver
-
 
 class DummyRPC(object):
 
@@ -36,96 +31,3 @@ class DummyRedis(object):
 
     def mset(self, kv={}):
         return None
-
-
-class DummyNodeExRoot(WebNodeExRoot):
-
-    def __init__(self):
-        self.redis = DummyRedis()
-        self.redis_driver = RedisFetchDriver(self.redis)
-        self.rpc_driver = RPCFetchDriver()
-        self.rpc = {
-            'dummy_rpc': DummyRPC()
-        }
-        super(DummyNodeExRoot, self).__init__(self.rpc, [self.redis_driver, self.rpc_driver])
-
-    def finish(self):
-        self.redis.mset(self.rpc_driver.get_self_kv())
-
-
-class Root(DummyNodeExRoot):
-
-    def __init__(self):
-        super(Root, self).__init__()
-
-    def children(self):
-        yield {
-            'node1': Node1(i=1),
-            'node2': Node2(),
-            'node3': Node3(),
-        }
-
-
-class NodePlus2(WebNodeEx):
-
-    def __init__(self, i):
-        super(NodePlus2, self).__init__()
-        self.i = i + 2
-
-    def register(self):
-        yield ObjectDict(
-            d1=self.dummy_rpc.call_by_id(self.i)
-        )
-
-
-class Node1(WebNodeEx):
-
-    def __init__(self, i):
-        super(Node1, self).__init__()
-        self.i = i
-
-    def register(self):
-        yield ObjectDict(
-            d1=self.dummy_rpc.call_by_id(self.i),
-            d2=self.dummy_rpc.call_by_id(2),
-            d3=[self.dummy_rpc.call_by_id(i) for i in range(1, 6)]
-        )
-
-    def children(self):
-        yield {
-            'node1': Node2(),
-            'nodes': [NodePlus2(i=x) for x in self.d3],
-        }
-
-
-class Node2(WebNodeEx):
-
-    def __init__(self):
-        super(Node2, self).__init__()
-
-    def register(self):
-        yield {
-            'd1': self.dummy_rpc.call_by_id(1),
-            'd2': self.dummy_rpc.call_by_id(3),
-        }
-
-    def children(self):
-        yield {}
-
-
-class Node3(WebNodeEx):
-
-    def __init__(self):
-        super(Node3, self).__init__()
-
-    def register(self):
-        yield {
-            'd1': self.dummy_rpc.call_by_id(2),
-            'd2': self.dummy_rpc.call_by_id(4),
-        }
-        yield {
-            'd3': self.dummy_rpc.call_by_id(self.d1),
-        }
-
-    def children(self):
-        yield {}
---
+++ webnodeex/largedummy.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tornado.util import ObjectDict
+
+from drivers import RedisFetchDriver, RPCFetchDriver
+from dummy import DummyRPC, DummyRedis
+from ex import LargeYieldNode, LargeYieldRoot
+
+
+class DummyLargeYieldRoot(LargeYieldRoot):
+
+    def __init__(self):
+        self.redis = DummyRedis()
+        self.redis_driver = RedisFetchDriver(self.redis)
+        self.rpc_driver = RPCFetchDriver()
+        self.rpc = {
+            'dummy_rpc': DummyRPC()
+        }
+        super(DummyLargeYieldRoot, self).__init__(self.rpc, [self.redis_driver, self.rpc_driver])
+
+    def finish(self):
+        self.redis.mset(self.rpc_driver.get_self_kv())
+
+
+class Root(DummyLargeYieldRoot):
+
+    def __init__(self):
+        super(Root, self).__init__()
+
+    def children(self):
+        yield {
+            'node1': Node1(i=1),
+            'node2': Node2(),
+            'node3': Node3(),
+        }
+
+
+class NodePlus2(LargeYieldNode):
+
+    def __init__(self, i):
+        super(NodePlus2, self).__init__()
+        self.i = i + 2
+
+    def register(self):
+        yield ObjectDict(
+            d1=self.dummy_rpc.call_by_id(self.i)
+        )
+
+
+class Node1(LargeYieldNode):
+
+    def __init__(self, i):
+        super(Node1, self).__init__()
+        self.i = i
+
+    def register(self):
+        yield ObjectDict(
+            d1=self.dummy_rpc.call_by_id(self.i),
+            d2=self.dummy_rpc.call_by_id(2),
+            d3=[self.dummy_rpc.call_by_id(i) for i in range(1, 6)]
+        )
+
+    def children(self):
+        yield {
+            'node1': Node2(),
+            'nodes': [NodePlus2(i=x) for x in self.d3],
+        }
+
+
+class Node2(LargeYieldNode):
+
+    def __init__(self):
+        super(Node2, self).__init__()
+
+    def register(self):
+        yield {
+            'd1': self.dummy_rpc.call_by_id(1),
+            'd2': self.dummy_rpc.call_by_id(3),
+        }
+
+    def children(self):
+        yield {}
+
+
+class Node3(LargeYieldNode):
+
+    def __init__(self):
+        super(Node3, self).__init__()
+
+    def register(self):
+        yield {
+            'd1': self.dummy_rpc.call_by_id(2),
+            'd2': self.dummy_rpc.call_by_id(4),
+        }
+        yield {
+            'd3': self.dummy_rpc.call_by_id(self.d1),
+        }
+
+    def children(self):
+        yield {}
